716f76926d7963b398c3a87514b9bb1f9d5247dc
el-feo <jebcoleman@gmail.com>
2014-06-18 13:52:14 -0700

    Configure the mailer/redis, update the timezone and adjust the validation output

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,35 @@ module Marketing
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      :address              => ENV['SMTP_ADDRESS'],
+      :port                 => ENV['SMTP_PORT'].to_i,
+      :domain               => ENV['SMTP_DOMAIN'],
+      :user_name            => ENV['SMTP_USERNAME'],
+      :password             => ENV['SMTP_PASSWORD'],
+      :authentication       => ENV['SMTP_AUTH']
+    }
+
+    config.action_mailer.smtp_settings[:enable_starttls_auto] = true if ENV['SMTP_ENCRYPTION'] == 'starttls'
+    config.action_mailer.smtp_settings[:ssl] = true if ENV['SMTP_ENCRYPTION'] == 'ssl'
+    config.action_mailer.default_options = { from: ENV['ACTION_MAILER_DEFAULT_FROM'] }
+    config.action_mailer.default_url_options = { host: ENV['ACTION_MAILER_HOST'] }
+
+    redis_store_options = { host: ENV['CACHE_HOST'],
+                            port: ENV['CACHE_PORT'].to_i,
+                            db: ENV['CACHE_DATABASE'].to_i,
+                            namespace: 'marketing::cache'
+                          }
+
+    redis_store_options[:password] = ENV['CACHE_PASSWORD'] if ENV['CACHE_PASSWORD'].present?
+    config.cache_store = :redis_store, redis_store_options
+
+    # run `bundle exec rake time:zones:all` to get a complete list of valid time zone names
+    config.time_zone = ENV['TIME_ZONE'] unless ENV['TIME_ZONE'] == 'UTC'
+
+    # http://www.loc.gov/standards/iso639-2/php/English_list.php
+    config.i18n.default_locale = ENV['DEFAULT_LOCALE'] unless ENV['DEFAULT_LOCALE'] == 'en'
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
@@ -19,5 +48,14 @@ module Marketing
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+  end
+end
+
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  if html_tag =~ /\<label/
+    html_tag
+  else
+    errors = Array(instance.error_message).join(',')
+    %(#{html_tag}<p class="validation-error"> #{errors}</p>).html_safe
   end
 end
